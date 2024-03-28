a30303f01101a1608cf330849b20b20137e7812c
Nhuda <nu12ulhud4@gmail.com>
2024-03-28 12:24:25 +0700

    fix: format

--- src/configs/images.ts
+++ src/configs/images.ts
@@ -1,9 +1,9 @@
-import SingleLogo from '@assets/images/single_logo.png';
 import FullLogo from '@assets/images/full_logo.png';
+import SingleLogo from '@assets/images/single_logo.png';
 
 const images = {
-  SingleLogo,
   FullLogo,
+  SingleLogo,
 };
 
 export default images;
